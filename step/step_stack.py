075b6c1a29ba6ba330b530a289bd39f375f4ed32
me <me@me.com>
2023-05-04 10:09:22 +1200

    init

--- step/step_stack.py
+++ step/step_stack.py
@@ -6,6 +6,11 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
+    aws_stepfunctions as sfn,
+    aws_stepfunctions_tasks as tasks,
+    aws_lambda as lambda_,
+    aws_events as events,
+    aws_events_targets as targets,
 )
 
 
@@ -14,13 +19,23 @@ class StepStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "StepQueue",
-            visibility_timeout=Duration.seconds(300),
+        hello_function = lambda_.Function(self, "MyLambdaFunction",
+                                          code=lambda_.Code.from_inline("""
+                                exports.handler = (event, context, callback) => {
+                                    callback(null, "Hello World!");
+                                }"""),
+                                          runtime=lambda_.Runtime.NODEJS_12_X,
+                                          handler="index.handler",
+                                          timeout=Duration.seconds(25))
+
+        state_machine = sfn.StateMachine(self, "MyStateMachine",
+                                         definition=tasks.LambdaInvoke(self, "MyLambdaTask",
+                                            lambda_function=hello_function).next(
+                                            sfn.Succeed(self, "GreetedWorld"))) 
+
+        event = events.Rule(self, "ScheduleRule",
+                            # default fror this cron method will just give every minute
+                            schedule=events.Schedule.cron(),
         )
 
-        topic = sns.Topic(
-            self, "StepTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
+        event.add_target(targets.SfnStateMachine(machine=state_machine))
